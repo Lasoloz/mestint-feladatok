31ffcf841c73cd8303150ad1b2b66ac6f500b162
Lasoloz <heim97laszlo@gmail.com>
2018-05-16 16:55:58 +0300

    [amoba] Minor correction in field evaluation

--- lab03-amoba/include/game/netPlay.hpp
+++ lab03-amoba/include/game/netPlay.hpp
@@ -2,8 +2,13 @@
 #define NETPLAY_INCLUDED
 
 #include <iostream>
+#include <tcp/TcpSocket.hpp>
+#include <tcp/TcpException.hpp>
+#include <tcp/TcpListener.hpp>
 
 void listenForEnemy();
 void connectToEnemy();
+
+void startup(TcpSocket &socket);
 
 #endif
--- lab03-amoba/src/game/netPlay.cpp
+++ lab03-amoba/src/game/netPlay.cpp
@@ -1,5 +1,24 @@
 #include <game/netPlay.hpp>
 
-void listenForEnemy() { std::cout << "Unimplemented!\n"; }
 
-void connectToEnemy() { std::cout << "Unimplemented!\n"; }
+void listenForEnemy() {
+    TcpListener listener;
+    try {
+        listener.open(80, TcpListener::M_AF_IPV4);
+        TcpSocket socket = listener.acceptConn();
+
+        startup(socket);
+    } catch (TcpException &ex) {
+        std::cerr << "Failed to open TCP listener: " << ex.what() << '\n';
+    }
+}
+
+void connectToEnemy() {
+    // TcpSocket socket;
+    std::cout << "Unimplemented!\n";
+}
+
+
+void startup(TcpSocket &socket) {
+    std::cout << "Unimplemented!\n";
+}
